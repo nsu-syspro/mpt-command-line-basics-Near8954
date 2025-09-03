c2fe070a635f89f226edab20b8183e32e343e0c2
Roman Baldin <roma.baldin@gmail.com>
2025-09-03 22:34:29 +0700

    task1 upd3 changed final directory

--- solution/task1.sh
+++ solution/task1.sh
@@ -1,5 +1,6 @@
 mkdir archive-combined
 unzip -u ./data/archive-part2.zip -d archive-combined
 tar -xf ./data/archive-part1.tar -C archive-combined
-tar -cf archive-combined.tar.gz archive-combined
+tar -cf archive-combined.tar.gz archive-combined/some
+mv archive-combined.tar.gz data
 rm -rf archive-combined
