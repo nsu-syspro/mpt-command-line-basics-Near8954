e78534a3a8229d7c0d274011b93bc80d9e9233b8
Roman Baldin <roma.baldin@gmail.com>
2025-09-03 22:15:56 +0700

    task 1

---
+++ solution/task1.sh
@@ -0,0 +1,5 @@
+mktemp -d archive-combined
+unzip -u ./data/archive-part2.zip -d archive-combined
+tar -xf ./data/archive-part1.tar -C archive-combined
+tar -cf archive-combined.tar.gz archive-combined
+rm -rf archive-combined
