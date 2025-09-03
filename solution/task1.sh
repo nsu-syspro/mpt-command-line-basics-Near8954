5770e7f87441b0057548b5c3952f8667fefede9a
Roman Baldin <roma.baldin@gmail.com>
2025-09-03 22:22:55 +0700

    task1 upd2 removed -d

--- solution/task1.sh
+++ solution/task1.sh
@@ -1,4 +1,4 @@
-mkdir -d archive-combined
+mkdir archive-combined
 unzip -u ./data/archive-part2.zip -d archive-combined
 tar -xf ./data/archive-part1.tar -C archive-combined
 tar -cf archive-combined.tar.gz archive-combined
